759efa35b24f6a24130eeb066b30644f42c220f3
fadhil.alfatayah <fadhil.alfatayah@gmail.com>
2022-07-10 16:35:53 +0700

    Add font Rubik

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -1,9 +1,11 @@
 export const fonts = {
   primary: {
     bold: 'FSAlbert-Bold',
+    extra_bold: 'FSAlbert-ExtraBold',
     normal: 'FSAlbert',
   },
 };
 
 export const FSAlbert = 'FSAlbert';
 export const FSAlbertBold = 'FSAlbert-Bold';
+export const FSAlbertExtraBold = 'FSAlbert-ExtraBold';
